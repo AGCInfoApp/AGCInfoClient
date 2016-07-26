ce9d8ffa0548d9e90859752eedd13c6b0a67c73e
bingbing19940623 <bingbing19940623@jinqimin>
2016-07-27 00:39:09 +0800

    加入接口、完成登录、完成我的主页、完成一部分我的信息

--- javascript/page/mine/myInfo.js
+++ javascript/page/mine/myInfo.js
@@ -1,9 +1,30 @@
+var url = "http://139.129.25.229:31010/";
 $(document).ready(function() {
-
-	if(window.sessionStorage) {
-		var token = sessionStorage.getItem("token");
+    
+	if(window.localStorage) {
+		var myToken = localStorage.getItem("myToken");
+		var myUserId = localStorage.getItem("myUserId");
 	}
-
+    
+    $.ajax({
+		type: "GET",
+		url: url + "prometheus/user/getInfo?userId=" + myUserId + "&token=" + myToken,
+		dataType: 'JSON',
+		beforeSend: function(XMLHttpRequest) {},
+		success: function(data, textStatus) {
+			var errCode = data["errCode"];
+			if(errCode == 0) {
+				$("#myNickname").html(data["data"].nickname);
+				$("#myUsername").html(data["data"].username);
+				$("#mySex").html(data["data"].sex);
+				$("#myPersonalSign").html("2333333333!!!");
+				
+			} else {
+				mui.toast(data["msg"]);
+			}
+		}
+	});
+    
 	document.getElementById("myInfoChangePicture").addEventListener("tap", function() {
 		mui.openWindow({
 			id: "myInfoChangePicture",
