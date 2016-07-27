2f2e49144bfc98aea17e878314ca1377422c4c56
hq-pc <hq-pc@hq-pc-xyl.zhongronglife.com>
2016-07-27 18:03:53 +0800

    生日

---
+++ javascript/page/mine/myInfoChangeBirthday.js
@@ -0,0 +1,65 @@
+var url = "http://139.129.25.229:31010/";
+$(document).ready(function() {
+
+	if(window.localStorage) {
+		var myToken = localStorage.getItem("myToken");
+		var myUserId = localStorage.getItem("myUserId");
+	}
+    
+    var container = [];
+    $.ajax({
+		type: "GET",
+		url: url + "prometheus/user/getInfo?userId=" + myUserId + "&token=" + myToken,
+		dataType: 'JSON',
+		beforeSend: function(XMLHttpRequest) {},
+		success: function(data, textStatus) {
+			var errCode = data["errCode"];
+			if(errCode == 0) {
+				$("#myNickname").attr("value",data["data"].nickname);
+				container = data["data"];
+			} else {
+				mui.toast(data["msg"]);
+			}
+		}
+	});
+	
+	document.getElementById("save").addEventListener('tap', function() {
+		var name = $.trim($("#nickname").val());
+		if(name.length < 15) {
+			var btnArray = ['是', '否'];
+			mui.confirm('是否确认保存？', '', btnArray, function(e) {
+				if(e.index == 1) {
+					//否
+				} else {
+					//是
+					$.ajax({
+						type: "POST",
+						url: url + "prometheus/user/editInfo",
+						dataType: 'JSON',
+						data: JSON.stringify({
+							'newName': name,
+						}),
+						beforeSend: function(XMLHttpRequest) {},
+						success: function(data, textStatus) {
+							var errCode = data["errCode"];
+							if(errCode == 0) {
+								mui.toast("保存成功");
+							} else {
+								mui.toast("保存失败，稍后重试…");
+							}
+						},
+						complete: function(XMLHttpRequest, textStatus) {
+
+						},
+						error: function() { //请求出错处理
+						}
+					});
+				}
+			});
+		}
+		else{
+			mui.toast("名字过长");
+		}
+	});
+
+});
